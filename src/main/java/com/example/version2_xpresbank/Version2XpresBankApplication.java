584a226451e82239b8346dc219dd3f9d013b9160
SOUFIANE-BOUSHABA <soufianboushaba12@gmail.com>
2024-12-03 11:35:03 +0100

    first commit

---
+++ src/main/java/com/example/version2_xpresbank/Version2XpresBankApplication.java
@@ -0,0 +1,13 @@
+package com.example.version2_xpresbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Version2XpresBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Version2XpresBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/version2_xpresbank/Version2XpresBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.version2_xpresbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Version2XpresBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
